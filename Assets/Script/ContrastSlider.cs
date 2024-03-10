fc79146e051f59ea6deb832d7647681792e6722c
Phuong Nguyen <haphuong311202@gmail.com>
2024-03-10 20:23:15 +0100

    Contrast Slider

--- Assets/Script/ContrastSlider.cs
+++ Assets/Script/ContrastSlider.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ContrastSlider : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Slider intensitySlider;
+    public Light directionalLight;
+
+    // Set the minimum and maximum values for the slider
+    [SerializeField] private float minIntensity = 1.5f;
+    [SerializeField] private float maxIntensity = 5f;
+
     void Start()
     {
-        
+        intensitySlider = GetComponent<Slider>();
+        if (intensitySlider == null || directionalLight == null)
+        {
+            Debug.LogError("Assign the slider and directional light in the inspector!");
+            return;
+        }
+
+        // Add a listener to the slider to update the light intensity
+        intensitySlider.onValueChanged.AddListener(UpdateLightIntensity);
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateLightIntensity(float intensity)
     {
-        
+        directionalLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, intensitySlider.value);
     }
 }
+
