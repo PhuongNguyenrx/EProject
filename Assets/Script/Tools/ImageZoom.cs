e1bdf44f6f729cab77f28d8073a540c0cdc21595
Phuong Nguyen <haphuong311202@gmail.com>
2024-04-18 21:24:27 +0200

    TapToZoom

--- Assets/Script/Tools/ImageZoom.cs
+++ Assets/Script/Tools/ImageZoom.cs
@@ -1,54 +1,29 @@
+using System.Runtime;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
+[RequireComponent (typeof(Image))]
 public class ImageZoom : MonoBehaviour
 {
+    Sprite image;
+    [SerializeField] GameObject targetPanel;
     private bool isZoomedIn = false;
-    private Vector3 originalScale;
-    private Vector2 originalPosition;
 
     void Start()
-    {
-        // Store the original scale and position of the image
-        originalScale = transform.localScale;
-        originalPosition = GetComponent<RectTransform>().anchoredPosition;
+    { 
+        image = GetComponent<Image>().sprite;
 
         // Add a click listener to the image
         GetComponent<Button>().onClick.AddListener(Zoom);
-    }
-
-    private void Update()
-    {
         
     }
-    void Zoom()
+    public void Zoom()
     {
-        if (GetComponent<RectTransform>().rect.height >= Screen.height)
-            return;
         // Toggle zoom state
+        targetPanel.GetComponent<Button>().onClick.AddListener(Zoom);
         isZoomedIn = !isZoomedIn;
-
-        // Zoom in or out based on the current zoom state
-        if (isZoomedIn)
-        {
-            // Calculate the scale factor to fill the screen vertically
-            float targetHeight = Screen.height;
-            float imageHeight = GetComponent<RectTransform>().rect.height;
-            float scaleFactor = targetHeight / imageHeight;
-
-            // Apply the scale factor to the image's scale
-            transform.localScale = originalScale * scaleFactor;
-
-            // Center the image on the screen
-            GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
-        }
-        else
-        {
-            // Zoom out
-            transform.localScale = originalScale;
-            // Reset the image position
-            GetComponent<RectTransform>().anchoredPosition = originalPosition;
-        }
+        targetPanel.GetComponent<Image>().sprite = image;
+        targetPanel.SetActive(isZoomedIn);
     }
 }
