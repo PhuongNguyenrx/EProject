67b038b3fc31e0f69ca51a213e9b45058e45caa6
Phuong Nguyen <haphuong311202@gmail.com>
2024-05-13 22:56:39 +0200

    Update third screen game + LineAnimator

--- Assets/Script/MiniGames/SpotGame.cs
+++ Assets/Script/MiniGames/SpotGame.cs
@@ -1,36 +1,76 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Device;
-using UnityEngine.UI;
 
 public class SpotGame : MiniGame
 {
-    [SerializeField] Image image;
+    LineAnimator lineAnimator;
+    [SerializeField] float timeInterval = 0.1f;
+
     private void Start()
     {
-        GetComponent<Button>().onClick.AddListener(Skip);
-        image = GetComponent<Image>();
+       lineAnimator = GetComponent<LineAnimator>();
     }
+    private void Update()
+    {
+        if (isSolved)
+            return;
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            var touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
 
-   
+            // Check if the ray hits any 2D colliders
+            if (hit.collider != null)
+                Skip();
+            
+        }
+    }
+    IEnumerator FadeImage()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        // Get the color value of the main material
+        Color color = spriteRenderer.materials[0].color;
+
+        if (!isSolved)
+            // While the color's alpha value is above 0
+            while (color.a > 0)
+            {
+                // Reduce the color's alpha value
+                color.a -= 0.1f;
+
+                // Apply the modified color to the object's mesh renderer
+                spriteRenderer.materials[0].color = color;
+
+                // Wait for the frame to update
+                yield return new WaitForSeconds(timeInterval);
+            }
+        else
+            while (color.a < 1)
+            {
+                // Reduce the color's alpha value
+                color.a += 0.1f;
+
+                // Apply the modified color to the object's mesh renderer
+                spriteRenderer.materials[0].color = color;
+
+                // Wait for the frame to update
+                yield return new WaitForSeconds(timeInterval);
+            }
+        // If material completely transparent or completely opaque, end coroutine
+        yield return new WaitUntil(() => isSolved ? spriteRenderer.materials[0].color.a >= 1f : spriteRenderer.materials[0].color.a <= 0);
+        yield return isSolved = true;
+    }
     public override void ResetGame()
     {
-        Color imageColor = image.color;
-        imageColor.a = 1;
-        image.color = imageColor;
+        StartCoroutine(FadeImage());
         isSolved = false;
+        lineAnimator.Disable();
     }
 
     public override void Skip()
     {
-        if (isSolved)
-            return;
-        Color imageColor = image.color;
-        imageColor.a = 0;
-        image.color = imageColor;
-        isSolved = true;
+        lineAnimator.DrawLine();
+        StartCoroutine(FadeImage());
     }
-
-   
 }
